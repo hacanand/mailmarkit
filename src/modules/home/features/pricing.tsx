b724f52961072f01b014ae30a5e2fcb2f53fdec9
anand <anandpromax@gmail.com>
2024-04-10 16:18:42 +0530

    Refactor branding and pricing components in home features

--- src/modules/home/features/pricing.tsx
+++ src/modules/home/features/pricing.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Button } from '@nextui-org/react'
 import React,{useState} from 'react'
 
 const Pricing = () => {
@@ -8,8 +9,13 @@ const Pricing = () => {
           <div className='w-[95%] m-auto py-5 '>
               <div className='w-full md:flex justify-between'>
                   <div>
-                      <h3 className='font-classDisplay text-center lg:text-left'></h3>
+                      <h3 className='font-clashDisplay text-center lg:text-left uppercase text-cyber-ink text-[2.75rem] md:text-7xl lg:text-[4rem] xl:text-[5.75rem] max-w-4xl '>Pricing</h3>
+                      <p className='text-3xl'>Simple. Predictable. Built for you</p>
                   </div>
+                  <div className='flex items-center mt-2 md:mt-0'>
+                      <Button className={ `${active==='Monthly'? 'bg-[#3843d0] text-white ':'bg-white text-black' } rounded-r-[0] !p-7 text-2xl !px-16 `}></Button>
+                  </div>
+
               </div>
           </div>
     </div>
