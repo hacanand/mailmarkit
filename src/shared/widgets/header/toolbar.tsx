1d809ae2f1753262e228ec470cc71d737116e81a
anand <anandpromax@gmail.com>
2024-04-11 00:35:32 +0530

    Update dependencies and refactor toolbar component

--- src/shared/widgets/header/toolbar.tsx
+++ src/shared/widgets/header/toolbar.tsx
@@ -1,18 +1,28 @@
-'use client';
-import { Button, Link } from '@nextui-org/react';
-import React from 'react'
+"use client";
+import { Button, Link } from "@nextui-org/react";
+import { useUser } from "@clerk/nextjs";
+import React from "react";
+import Image from "next/image";
 
 const toolbar = () => {
-    return (
-      <>
-        <Button color="primary" className="text-lg font-semibold">
-          Start Trial
-        </Button>
-        <Link href={"/sign-up"} className="text-lg font-semibold border-2 p-1 px-2 border-blue-600 rounded-xl hover:bg-blue-100">
+  const { user } = useUser();
+  return (
+    <>
+      <Button color="primary" className="text-lg font-semibold">
+        Start Trial
+      </Button>
+      {user ?
+        <>
+          <Link href={'/dashboard'}>
+            <Image src={user?.imageUrl} alt="profile" width={40} height={40} className="rounded-full" />
+          </Link>
+        </> : <Link href="/sign-in">login</Link>
+      }
+      {/* <Link href={"/sign-up"} className="text-lg font-semibold border-2 p-1 px-2 border-blue-600 rounded-xl hover:bg-blue-100">
           Login
-        </Link>
-      </>
-    );
-}
+        </Link> */}
+    </>
+  );
+};
 
-export default toolbar
+export default toolbar;
