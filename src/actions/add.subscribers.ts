f370481231ac1cdd8aa53343241cf6005b983eb5
anand <anandpromax@gmail.com>
2024-04-14 00:44:46 +0530

    Refactor subscribe function in add.subscribers.ts

--- src/actions/add.subscribers.ts
+++ src/actions/add.subscribers.ts
@@ -3,6 +3,7 @@ import Subscriber from "@/models/subscribers.model";
 import { connectDb } from "@/shared/libs/db";
 import validateEmail from "@/shared/utils/VerifaliaApi";
 import { clerkClient } from "@clerk/nextjs";
+
 export const subscribe = async ({
   email,
   username,
@@ -13,19 +14,34 @@ export const subscribe = async ({
   try {
     await connectDb();
     //find the user
-    const newsletterOwner = await clerkClient.users.getUser(username);
+    // first we need to fetch all users
+    const allUsers = await clerkClient.users.getUserList();
+    // now we need to find our newsletter owner
+    const newsletterOwner = allUsers.find((i) => i.username === username);
+
+    if (!newsletterOwner) {
+      throw Error("Username is not valid!");
+    }
+    //check if user is already subscribed
     const isSubscribed = await Subscriber.findOne({
       email,
       newsLetterOwnerId: newsletterOwner?.id,
     });
-      if(isSubscribed){
-          return { error: "You are already subscribed" };
-      }
-      //validate the email
-      const validationResponse = await validateEmail({ email });
-        if (validationResponse?.classification === "Undeliverable") {
-          return { error: "Undeliverable email address" };
-        }
+    if (isSubscribed) {
+      return { error: "You are already subscribed" };
+    }
+    //validate the email
+    const validationResponse = await validateEmail({ email });
+    if (validationResponse?.classification === "Undeliverable") {
+      return { error: "Email is not valid" };
+    }
+
+    //create the subscriber
+      const subscriber = await Subscriber.create({
+        email,
+        newsLetterOwnerId: newsletterOwner?.id,
+      });
+      return subscriber;
   } catch (error) {
     console.error("Error subscribing user: ", error);
   }
